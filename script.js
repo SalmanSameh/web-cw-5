7d685d350228bf27015f3d60f7e2ee93983adfe5
SalmanSameh <salmants101@gmail.com>
2022-03-16 19:05:28 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,7 @@ alert("My name is Salman");
 console.log("%c My age is 18", 'color: red');
 console.log("%c I'm in grade 12", 'color: red');
 // جافاسكريبت ليست كجافا
+let budget = 20;
+console.log(budget > 30 && budget < 80);
+budget += 30;
+console.log(budget > 30 && budget < 80);
